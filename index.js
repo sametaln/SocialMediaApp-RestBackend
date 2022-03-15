76bc492694873984d1be6d690846351276371106
sametaln <a.sametalan56@gmail.com>
2022-03-15 16:34:48 +0300

    15.03

--- index.js
+++ index.js
@@ -21,6 +21,15 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan('common'));
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
+
 app.use('/api/users', userRoute);
 app.use('/api/auth', authRoute);
 app.use('/api/posts', postRoute);
